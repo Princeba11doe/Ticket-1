6e3f57ad53e93ed6590cad4590316ead36d9e158
Prince Baidoe <Princeba11doe@gmail.com>
2025-01-29 11:13:21 -0500

    Initial Commit

---
+++ src/main/java/Service/DoctorService.java
@@ -0,0 +1,39 @@
+package Service;
+
+import com.healthcaremanagement.model.Model.Doctor;
+import com.healthcaremanagement.model.repository.DoctorRepositoryImpl;
+
+import java.util.List;
+
+public class DoctorService {
+
+    private final DoctorRepositoryImpl doctorRepository;
+
+    public DoctorService(DoctorRepositoryImpl doctorRepository) {
+        this.doctorRepository = doctorRepository;
+    }
+
+    public void createDoctor(Doctor doctor){
+        doctorRepository.createDoctor(doctor);
+
+    }
+
+    public Doctor getDoctorByID(int doctorId){
+       return doctorRepository.getDoctorById(doctorId);
+
+    }
+
+    public void updateDoctor(Doctor doctor){
+        doctorRepository.updateDoctor(doctor);
+
+    }
+
+    public void deleteDoctor(int doctorId){
+        doctorRepository.deleteDoctor(doctorId);
+    }
+
+    public List<Doctor> getAllDoctors(){
+        return doctorRepository.getAllDoctor();
+    }
+
+}
